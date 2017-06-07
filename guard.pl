10f7281096e7c6299817ec0e302c3360c6d7df32
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-06-07 15:26:28 +0200

    Renaming and restructuring

--- guard.pl
+++ guard.pl
@@ -1,5 +1,39 @@
-:- module(swimon,
-	  [ swimon/0
+/*  Part of SWI-Prolog
+
+    Author:        Jan Wielemaker
+    E-mail:        J.Wielemaker@vu.nl
+    WWW:           http://www.swi-prolog.org
+    Copyright (c)  2017, VU University Amsterdam
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in
+       the documentation and/or other materials provided with the
+       distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+*/
+
+:- module(swiguard,
+	  [ swiguard/0
 	  ]).
 :- use_module(library(settings)).
 :- use_module(library(process)).
@@ -14,9 +48,9 @@
 :- debug(monitor(_)).
 :- debug_message_context(+time('%D %T.%3f')).
 
-:- initialization swimon.
+:- initialization(swiguard, main).
 
-swimon :-
+swiguard :-
 	monitor_loop.
 
 monitor_loop :-
@@ -41,15 +75,18 @@ monitor(Service) :-
 
 alive(Service) :-
 	Test = Service.test,		% FIXME: must be fixed in goal expansion
+	get_time(T0),
 	(   Timeout = Service.get(timeout)
 	->  catch(call_with_time_limit(
 		      Timeout,
-		      call(Test, Service)), E,
+		      call(Test, Service, Reply)), E,
 		  ( print_message(error, E), fail))
-	;   catch(call(Test, Service), E,
+	;   catch(call(Test, Service, Reply), E,
 		  ( print_message(error, E), fail))
 	),
-	debug(monitor(test), '~w: alive', [Service.name]).
+	get_time(T1),
+	T is T1-T0,
+	debug(monitor(test), '~w: alive (~3f sec; ~p)', [Service.name, T, Reply]).
 
 restart(Service) :-
 	Job = Service.get(service), !,
@@ -65,7 +102,7 @@ restart(Service) :-
 
 service(_{ name:swish,
 	   port:80,
-%	   host:'swish.swi-prolog.org',
+	   host:'swish.swi-prolog.org',
 	   test:test_pengines,
 	   timeout:20,
 	   service:swish
@@ -107,10 +144,10 @@ service_url(Service, URL) :-
 		 *      SPECIFIC SERVICES	*
 		 *******************************/
 :- public
-	test_pengines/1.
+	test_pengines/2.
 
-test_pengines(Service) :-
+test_pengines(Service, V) :-
 	service_url(Service, URL),
-	pengine_rpc(URL, current_prolog_flag(version, V)),
+	pengine_rpc(URL, statistics(threads, V)),
 	integer(V).
 
